37a2e1c985124b6b2deca77dbcadddbb6aabbd85
Noni <noni.levinson@gmail.com>
2021-06-15 12:47:43 -0300

    12h47

--- a_duraca_frm.php
+++ a_duraca_frm.php
@@ -2,7 +2,7 @@
 
 echo
 "<table class='tabFormulario'>",
-	$this->Pedir( "Médico", Pessoa ),
+	$this->Pedir( "Médico", Usuario ),
    $this->Pedir( "Válido a partir de", Inicio ),
    $this->Pedir( "Duração de cada homologação",
       [ " ", Duracao, " min (1, 2, 3, 4, 5, 6, 10, 12, 15, 20, 30 ou 60 minutos; 0 = não atende mais)" ] ),
