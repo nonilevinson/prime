6f5b01981ad14c38a067de6b9d047bacbfb87d75
Noni <noni.levinson@gmail.com>
2021-07-05 15:17:35 -0300

    15h17

--- a_duraca_frm.php
+++ a_duraca_frm.php
@@ -3,9 +3,10 @@
 echo
 "<table class='tabFormulario'>",
 	$this->Pedir( "Clínica", Clinica ),
-   $this->Pedir( "Válido a partir de", Inicio ),
-   $this->Pedir( "Duração de cada homologação",
-      [ " ", Duracao, " min (1, 2, 3, 4, 5, 6, 10, 12, 15, 20, 30 ou 60 minutos; 0 = não atende mais)" ] ),
+   $this->Pedir( "Válido a partir de",
+      [ "", Inicio, " (precisa ser um Domingo)" ] ),
+   $this->Pedir( "Duração de cada consulta",
+      [ " ", Duracao, " min (10, 15, 20, 30 ou 60 minutos)" ] ),
    $this->Pedir( "Agendamento disponível para até",
       [ " ", MaxAgenda, " dias a partir de hoje" ] ),
 "</table>";
