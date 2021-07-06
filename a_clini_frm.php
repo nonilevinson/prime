7d8a34accfc25da923a65d79f4b82ddc9a6c7a50
Noni <noni.levinson@gmail.com>
2021-07-06 12:11:35 -0300

    12h11

--- a_clini_frm.php
+++ a_clini_frm.php
@@ -8,25 +8,13 @@ echo
    $this->Pedir( "Razão social", Razao ),
    $this->Pedir( "Email" ),
    $this->Pedir( "CNPJ" ),
+   $this->Pedir( "Agendamento disponível para até",
+      [ " ", MaxAgenda, " dias a partir de hoje" ] ),
    $this->Pedir( "Ativa?", Ativo ),
 "</table>",
 
 CriarForms(
-	[ 'Horário', 'H', true ],
-   [ 'Endereço', 'E', true ] ),
-
-//* Horario
-"<table id='H' class='tabFormulario' style='display:none'>",
-/*
-   $this->Pedir( "Agendamento disponível para até",
-      [ " ", MaxAgenda, " dias a partir de hoje" ] ),
-*/
-   $this->Pedir( "Horário",
-      [ "", HoraIni,
-      [ " h às ", HoraFim, " h" ] ] ),
-   $this->Pedir( "Sábado?", ConsSab ),
-   $this->Pedir( "Domingo?", ConsDom ),
-"</table>",
+	[ 'Endereço', 'E', true ] ),
 
 //* Endereço
 "<table id='E' class='tabFormulario' style='display:none'>",
