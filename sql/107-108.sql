36ed3ce8531876bfffe077c57ad9b3033ff1c2e6
Noni <noni.levinson@gmail.com>
2021-09-11 08:57:13 -0300

    08h57

--- sql/107-108.sql
+++ sql/107-108.sql
@@ -7,4 +7,24 @@ commit;
 execute procedure reindexartudo;
 commit;
 
+insert into arqLanceOperacao values(100046,1,'Cadastro de contas recorrentes a pagar e a receber','arqRecorrente',46,10,0,'');
+commit;
 
+/************************************************************
+	TABELA tabTCompete
+************************************************************/
+
+CREATE TABLE tabTCompete
+(
+	IDPRIMARIO chavePrimariaTab,
+	CHAVE VARCHAR( 1 ) COLLATE PT_BR,
+	DESCRITOR VARCHAR( 75 ) COLLATE PT_BR,
+	CONSTRAINT tabTCompete_PK PRIMARY KEY( IDPRIMARIO ),
+	CONSTRAINT tabTCompete_UK UNIQUE( CHAVE )
+);
+commit;
+
+INSERT INTO tabTCompete VALUES ( 1, '1', 'Anterior' );
+INSERT INTO tabTCompete VALUES ( 2, '2', 'Atual' );
+INSERT INTO tabTCompete VALUES ( 3, '3', 'Próximo' );
+commit;
