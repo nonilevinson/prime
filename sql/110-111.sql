99ba47363bde509f1ca990df8194dc38103688bc
Noni <noni.levinson@gmail.com>
2021-10-11 10:59:49 -0300

    10h59

---
+++ sql/110-111.sql
@@ -0,0 +1,10 @@
+--*
+--* 1.10 para 1.11
+
+delete from ARQLANCELOGACESSO where STATUS is null;
+delete from ARQLANCELOGACESSO where datediff(year, Data, current_date) > 5;
+commit;
+execute procedure reindexartudo;
+commit;
+
+
