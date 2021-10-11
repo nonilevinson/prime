86a5dd5ffac309f467c879af379ae02a50129617
Noni <noni.levinson@gmail.com>
2021-10-11 11:18:22 -0300

    11h18

--- a_profis_frm.php
+++ a_profis_frm.php
@@ -3,4 +3,5 @@
 echo
 "<table class='tabFormulario'>",
 	$this->Pedir( "Profissão", Profissao ),
+	$this->Pedir( "Ativo?", Ativo ),
 "</table>";
--- sql/110-111.sql
+++ sql/110-111.sql
@@ -7,4 +7,54 @@ commit;
 execute procedure reindexartudo;
 commit;
 
+/************************************************************
+	Arquivo Profissao 
+************************************************************/
+drop trigger arqProfissao_log;
+drop view v_arqProfissao;
+commit;
 
+ALTER TABLE arqProfissao
+add /*  3*/	ATIVO campoLogico; /* Lógico: 0=Não 1=Sim */
+commit;
+
+update arqProfissao set Ativo = 1;
+commit;
+
+RECREATE VIEW V_arqProfissao AS 
+	SELECT A0.IDPRIMARIO, A0.PROFISSAO, A0.ATIVO
+	FROM arqProfissao A0;
+commit;
+
+/************************************************************
+	Trigger para Log de arqProfissao
+************************************************************/
+
+set term ^;
+
+recreate trigger arqProfissao_LOG for arqProfissao
+active after Insert or Delete or Update
+position 999
+as
+	declare variable valorChave varchar(1000);
+begin
+if( deleting ) then
+	valorChave = coalesce( OLD.Profissao,'' );
+else
+	valorChave = coalesce( NEW.Profissao,'' );
+rdb$set_context( 'USER_SESSION', 'IDOPERACAO', 100036 );
+rdb$set_context( 'USER_SESSION', 'VALORCHAVE', substring( valorChave from 1 for 255 ) );
+if( inserting ) then
+	execute procedure set_log( 13, NEW.idPrimario, null, null, null ); 
+else
+if( deleting ) then
+	execute procedure set_log( 14, OLD.idPrimario, null, null, null ); 
+else begin
+	execute procedure set_log( 12, NEW.idPrimario, 'Profissao', OLD.Profissao, NEW.Profissao );
+	execute procedure set_log( 12, NEW.idPrimario, 'Ativo', OLD.Ativo, NEW.Ativo );
+end
+end^
+
+set term ;^
+
+commit;
