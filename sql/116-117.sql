df378a9a43dcefba005e7390cf2ac3e48efbe254
Noni Levinson <noni.levinson@gmail.com>
2022-01-06 13:18:37 -0300

    13h18

---
+++ sql/116-117.sql
@@ -0,0 +1,10 @@
+--*
+--* 1.16 para 1.17
+
+delete from ARQLANCELOGACESSO where STATUS is null;
+delete from ARQLANCELOGACESSO where datediff(year, Data, current_date) > 5;
+commit;
+execute procedure reindexartudo;
+commit;
+
+
