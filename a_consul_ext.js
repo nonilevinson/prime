0eee9936605915012b939497fadfe51dd616176f
Noni Levinson <noni.levinson@gmail.com>
2022-02-01 12:43:48 -0300

    12h43

--- a_consul_ext.js
+++ a_consul_ext.js
@@ -35,6 +35,7 @@ function vijMedico()
          "join arqGrupo G on G.idPrimario=U.Grupo " +
       "Where U.idPrimario = A.idPrimario) = 1" );
 }
+
 //----------------------------------------------------------
 function vijAssessor()
 {
