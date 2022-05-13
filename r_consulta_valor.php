e35288d5fe44af7b9a8f89756576a11e6e473e8a
Noni Levinson <noni.levinson@gmail.com>
2022-05-13 17:04:34 -0300

    17h04

--- r_consulta_valor.php
+++ r_consulta_valor.php
@@ -14,20 +14,20 @@ class RelConsulta extends Relatorios
 			' ' ];
 
       $this->DefinirCabColunas(
-         [ "Nº", 	         18, ALINHA_CEN ],
+         [ "Nº", 	         17, ALINHA_CEN ],
          [ "Tipo",	      32, ALINHA_ESQ ],
-         [ "Paciente",     83, ALINHA_ESQ ],
+         [ "Paciente",     68, ALINHA_ESQ ],
          [ "Celular",	   27, ALINHA_CEN ], 
          [ "Prontuário",   25, ALINHA_CEN ],
          [ "Call Center",  30, ALINHA_ESQ ],
-         [ "Consulta",     30, ALINHA_DIR ],
-         [ "Tratamento",   30, ALINHA_DIR ]
+         [ "Consulta",     20, ALINHA_DIR ],
+         [ "Tratamento",   22, ALINHA_DIR ]
           );
 
       $this->DefinirQuebras(
          [ 'QuebraPorClinica',   SIM, NAO, SIM ] );
 
-		$this->DefinirTotais( "totConsultas" );
+		$this->DefinirTotais( "totConsultas", "totValor", "totValPTrata" );
 
 		$this->cabPaginaTemCabColunas = false;
 		$this->comCodigoRel           = false;
@@ -35,9 +35,14 @@ class RelConsulta extends Relatorios
 	}
 
 	//------------------------------------------------------------------------
-	function PeQuebra( $p_cabTotal, $p_colTotal=0 )
+	function PeQuebra( $p_cabTotal )
 	{
-      $this->ImprimirTotalEmUmaColuna( $p_cabTotal . ": " . $this->ValorTotal( "totConsultas" ) . " consultas");
+      $this->JuntarColunas( [0,5,ALINHA_ESQ] );
+		$this->valores[ 0 ] = $p_cabTotal . ": " . $this->FormatarTotal( "totConsultas" ) . " consultas";
+		$this->valores[ 6 ] = $this->FormatarTotal( "totValor", [ 2, '', '', ')' ] );
+		$this->valores[ 7 ] = $this->FormatarTotal( "totValPTrata", [ 2, '', '', ')' ] );
+		$this->ImprimirTotalColunas();
+		$this->RestaurarColunas();
 	}
 
 	//------------------------------------------------------------------------
@@ -47,8 +52,8 @@ class RelConsulta extends Relatorios
 		
 		if( !$parQSelecao->CLINICA )
 		{
-			$this->MarcarPosicao( 'TOTAL GERAL' );
-			$this->PeQuebra( 'TOTAL GERAL' );
+			$this->MarcarPosicao( 'Total geral' );
+			$this->PeQuebra( 'Total geral' );
 		}
       
       $this->deveriaFecharLinhas = 0; 
@@ -84,20 +89,24 @@ class RelConsulta extends Relatorios
 	function Basico()
 	{
 		$regA = &$this->regAtual;
+		$valor     = $regA->VALOR;
+		$valPTrata = $regA->VALPTRATA;
       
       $this->valores = [
          formatarNum( $regA->NUMCONSULTA ),
          $regA->TIAGENDA,
-         formatarHora( $regA->HORA, 'hh:mm' ),
-         cadEsq( $regA->NOME, 40 ),
+         cadEsq( $regA->NOME, 35 ),
          formatarStr( $regA->NUMCELULAR, '(nn) n.nnnn.nnnn' ),
          $regA->PRONTUARIO,
-			cadEsq( $regA->CALLCENTER, 14 )
-			 ];
+			cadEsq( $regA->CALLCENTER, 14 ),
+			formatarValor( $valor ),
+			formatarValor( $valPTrata ) ];
 
       $this->ImprimirValorColunas();
       
 		$this->AcumularTotal( "totConsultas", 1 );
+		$this->AcumularTotal( "totValor", $valor );
+		$this->AcumularTotal( "totValPTrata", $valPTrata );
 	}
 }
 
@@ -107,7 +116,7 @@ class RelConsulta extends Relatorios
 global $parQSelecao;
 $parQSelecao = lerParametro( "parQSelecao" );
 
-$proc = new RelConsulta( RETRATO, A4, 'Consultas_Relacao.pdf', '', true, .89 );
+$proc = new RelConsulta( RETRATO, A4, 'Consultas_Relacao.pdf', '', true, .85 );
 
 $filtro = substr(
    ( SQL_VETIDCLINICA ? "C.Clinica in " . SQL_VETIDCLINICA . ' and ': '' ) .
@@ -116,8 +125,8 @@ $filtro = substr(
 	filtrarPorLig( "C.TiAgenda", $parQSelecao->TIAGENDA ) .
 	filtrarPorLig( 'C.Clinica', $parQSelecao->CLINICA ), 0, -4 );
 
-$select = "Select L.Clinica, C.Num as NumConsulta, T.TiAgenda, C.Hora, P.Nome, P.Prontuario,
-      P.NumCelular, U.Nome as CallCenter
+$select = "Select L.Clinica, C.Num as NumConsulta, T.TiAgenda, P.Nome, P.Prontuario, P.NumCelular,
+		U.Nome as CallCenter, C.Valor, C.ValPTrata
 	From arqConsulta C
       join arqClinica   	L on L.idPrimario=C.Clinica
       join arqTiAgenda  	T on T.idPrimario=C.TiAgenda
