94f0014e2e71e078a4c21b8218bb22ff04ee0585
Noni Levinson <noni.levinson@gmail.com>
2022-08-02 12:53:16 -0300

    12h53

--- p_rotina1.php
+++ p_rotina1.php
@@ -5,53 +5,9 @@ global $g_debugProcesso;
 // sql_abrirBD( OperacaoAtual() );
 sql_abrirBD( false );
 
-
-sql_executarComando( 'alter trigger ARQCONSULTA_AI_AU inactive;' );
+sql_executarComando( 'delete from ARQLANCELOGACESSO where STATUS is null;' );
+sql_executarComando( 'delete from ARQLANCELOGACESSO where datediff(year, Data, current_date) > 5;' );
 sql_commit();
-
-
-$select = "Select C.idPrimario as idConta
-   From arqConta C
-   Where C.TrgValor = 0";
-$regConta =sql_lerRegistros( $select );
-echo '<br><b>GR0 arqConta S=</b> '.$select;
-
-foreach( $regConta as $umaConta )
-{
-   $idConta = $umaConta->IDCONTA;
-
-   sql_update( "arqConsulta", [
-         "ContaCons" => null ],
-      "ContaCons = " . $idConta,1,true );
-
-   $delete = "delete From arqConta Where idPrimario = " . $idConta;
-echo '<br><b>GR0 delete S=</b> '.$delete;
-   
-   sql_executarComando( $delete );
-   sql_commit();
-}
-
-sql_executarComando( 'alter trigger ARQCONSULTA_AI_AU active;' );
-sql_commit();
-
-/*
-$select = "Select RecorDia 
-   From cnfXConfig";
-$recorDia = sql_lerUmRegistro( $select )->RECORDIA;
-if( $g_debugProcesso ) echo '<br><b>GR0 cnfXConfig S=</b> '.$select.' <b>recorDia=</b> '.$recorDia;
-
-$hoje = formatarData( HOJE, 'aaaa/mm/dd');
-$diaHoje = dataDia( $hoje );
-$ultDia = ultDiaDoMes( $hoje );
-
-if( $g_debugProcesso ) echo '<br><b>GR0 hoje=</b> '.$hoje.' diaHoje= '.$diaHoje.' ult= '.$ultDia;
-echo '<br>';
-
-if( $recorDia == $diaHoje || $recorDia > $ultDia || ultimaLigOpcaoEm( 162 ) )
-   echo '<br>&nbsp;&nbsp;PODE';
-else
-   echo '<br>&nbsp;&nbsp;NÃO PODE';
-*/
 
 sql_fecharBD();
 
--- sql/202-203.sql
+++ sql/202-203.sql
@@ -1,6 +1,8 @@
 --*
 --* 2.02 para 2.03
 
+--* 02/08/2022 passei eles para o p_indexatua.php
+/*
 delete from ARQLANCELOGACESSO where STATUS is null;
 delete from ARQLANCELOGACESSO where datediff(year, Data, current_date) > 5;
 commit;
@@ -9,6 +11,13 @@ commit;
 --* vira e mexe tem prontuario como null
 update arqPessoa set Prontuario='' where Prontuario is null;
 commit;
+*/
 
 insert into arqLanceOperacao values(200281,2,'Relatório de pacientes por assessor','',281,50,0,'');
+commit;
+
+/************************************************************
+	TABELA tabTStCon
+************************************************************/
+drop TABLE tabTStCon;
 commit;
