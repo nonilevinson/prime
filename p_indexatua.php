88997d1c8579756d5d06ed68d90944c0f5a8eb82
Noni Levinson <noni.levinson@gmail.com>
2022-08-16 07:57:40 -0300

    07h57

--- p_indexatua.php
+++ p_indexatua.php
@@ -6,12 +6,16 @@ if( $g_debugProcesso ) echo '<br><b>GR0 INÍCIO </b> '.AGORA;
 sql_abrirBD( false );
 
 //* Esses ficavam no conversor e me toquei que podem ficar aqui e rodarem sempre pela Task
+$dia = dataDia( HOJE);
+if( $dia == 10 )
+{
    sql_executarComando( 'delete from ARQLANCELOGACESSO where STATUS is null;' );
    sql_executarComando( 'delete from ARQLANCELOGACESSO where datediff(year, Data, current_date) > 5;' );
    sql_commit();
    sql_executarComando( 'execute procedure reindexartudo;' );
    sql_commit();
-   
+}
+  
    sql_executarComando( "update arqPessoa set Prontuario='' where Prontuario is null;" );
    sql_commit();
 //* Fim
--- p_rotina1.php
+++ p_rotina1.php
@@ -3,13 +3,22 @@
 global $g_debugProcesso;
 
 // sql_abrirBD( OperacaoAtual() );
-sql_abrirBD( false );
+// sql_abrirBD( false );
 
-sql_executarComando( 'delete from ARQLANCELOGACESSO where STATUS is null;' );
-sql_executarComando( 'delete from ARQLANCELOGACESSO where datediff(year, Data, current_date) > 5;' );
-sql_commit();
+$dia = dataDia( HOJE);
+echo '<br>dia= '.$dia;
 
-sql_fecharBD();
+if( $dia == 15 )
+   echo '<br> eh dia 15';
+else
+   echo '<br>nao eh dia 15';
+
+if( $dia == 16 )
+   echo '<br> eh dia 16';
+else
+   echo '<br>nao eh dia 16';
+
+// sql_fecharBD();
 
 echo '<p style="text-align: center; font-weight: bold; font-size:16px">*** Fim às ' .
    formatarHora( AGORA, 'hh:mm' ) . ' ***</p>';
