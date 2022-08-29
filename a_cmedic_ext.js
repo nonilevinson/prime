93c938fe1faf29809ab48d4c192b600b5d913d1a
Noni Levinson <noni.levinson@gmail.com>
2022-08-29 13:30:48 -0300

    13h30

--- a_cmedic_ext.js
+++ a_cmedic_ext.js
@@ -2,9 +2,9 @@
 function vijLote()
 {
    return( 
-      ( g_inserindo ? "A.Ativo = 1 and A.Estoque > 0 " : "" ) +
+      ( g_inserindo ? "A.Ativo = 1 and A.Estoque > 0 and " : "" ) +
       ( g_temMaisDeUmClinica 
-         ? "and ( A.Clinica in (" + g_vetIdClinica + ") ) "
-         : "" )
+         ? "( A.Clinica in (" + g_vetIdClinica + ") ) "
+         : "1=1 " )
    );
 }
